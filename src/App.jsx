bb2ed6cf68266206363a52f4c410216f68d05292
Ons Khiari <onskhiari2001@gmail.com>
2024-03-12 12:08:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+import React, { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import NotFound from './components/NotFound'
+import NavigationBar from './components/NavigationBar'
+import EventDetails from './components/EventDetails'
+import AddEvent from './components/AddEvent'
+import UpdateEvent from './components/UpdateEvent'
+import { fetchEvents } from './redux/slices/eventsSlice'
+import { useDispatch } from 'react-redux'
+
+
+const Events = React.lazy(() => import('./components/Events'))
+function App() {
+
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <NavigationBar />
+      <Suspense fallback={<p>Loading ...</p>}>
+        <Routes>
+          <Route path="events">
+            <Route index element={<Events />} loader={dispatch(fetchEvents())} />
+            <Route path="add" element={<AddEvent />} />
+            <Route path="update/:id" element={<UpdateEvent />} />
+            <Route path="details/:id" element={<EventDetails />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UpdateEvent.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react'
+import { Button, Container, Form } from "react-bootstrap";
+import { addEvent, editEvent, getallEvents } from '../service/api';
+import { useNavigate, useParams } from 'react-router-dom';
+
+function UpdateEvent() {
+  const param = useParams()
+  const navigate = useNavigate()
+  const [eventItem, setEventItem] = useState({
+    name: "",
+    description: "",
+    img: "",
+    price: 0,
+    nbTickets: 0,
+    nbParticipants: 0,
+    like: false
+  })
+
+  const fetchEvent = async () => {
+    const eventResult = await getallEvents(param.id);
+    setEventItem(eventResult.data);
+  };
+
+  useEffect(() => {
+    fetchEvent(param.id);
+  }, []);
+
+  const onValueChange = (e) => {
+    setEventItem({ ...eventItem, [e.target.name]: e.target.value })
+  }
+
+  const onFile = (e) => {
+    setEventItem({ ...eventItem, [e.target.name]: e.target.files[0].name })
+  }
+
+  const UpdateEvent = async () => {
+    const eventResult = await editEvent(param.id, eventItem)
+    if (eventResult.status == 200) {
+      navigate("/events")
+    }
+  }
+
+  return (
+    <Container style={{ marginTop: "30px" }}>
+      <h2>Update  Event </h2>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            onChange={(e) => onValueChange(e)}
+            name="name"
+            value={eventItem.name}
+            type="text"
+            placeholder="Enter a Name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            onChange={(e) => onValueChange(e)}
+            value={eventItem.description}
+            as="textarea"
+            rows={3}
+            placeholder="Enter description "
+            name="description"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            onChange={(e) => onValueChange(e)}
+            value={eventItem.price}
+            type="number"
+            name="price"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Number of Tickets</Form.Label>
+          <Form.Control
+            onChange={(e) => onValueChange(e)}
+            value={eventItem.nbTickets}
+            type="number"
+            name="nbTickets"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Image</Form.Label>
+          <Form.Control
+            type="file"
+            name="img"
+            onChange={(e) => onFile(e)}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={UpdateEvent}>
+          Update
+        </Button>
+        <Button variant="secondary">
+          Cancel
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default UpdateEvent
